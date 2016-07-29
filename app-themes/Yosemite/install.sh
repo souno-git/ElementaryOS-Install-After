8be19e0d7a1612479afd6d119aa0c3b1b4390dc5
Shenya <88459122@qq.com>
2016-07-29 11:05:13 +0800

    123

--- app-themes/Yosemite/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "This script will ask for sudo rights in order to copy files to the theme directory."
-echo " "
-echo "Installing..."
-sudo cp -r minimum /boot/burg/themes
-echo "Setting permissions..."
-sudo chmod -R 755 /boot/burg/themes/minimum/
-echo "..."
-echo "Installation finished ! Enjoy ! :D"
